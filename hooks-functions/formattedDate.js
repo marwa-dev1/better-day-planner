7393b45004c85e53353292455b35ab912ab76a18
marwa-nabulsi <mar1_2005@hotmail.com>
2020-08-30 11:09:08 +0300

    second commit

--- hooks-functions/formattedDate.js
+++ hooks-functions/formattedDate.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//Format dates
 export const formattedDate = date => {
     const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'numeric', day: '2-digit' });
     const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(date); 
